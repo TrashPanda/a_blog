93b6c88674dd0e8fb06c9092fa4f1ff387494fe0
TrashPanda <sean1teo1@gmail.com>
2015-09-16 23:44:25 -0400

    heroku deployment

--- settings.js
+++
@@ -1,7 +0,0 @@
-//mongodb settings
-var dbSetting = {
-  cookieSecret: 'a_blog_twitch_chat',
-  url: 'mongodb://herokutest:herokutest@ds041673.mongolab.com:41673/a_blog'
-};
-
-module.exports = dbSetting;
