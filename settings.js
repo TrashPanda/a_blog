7ca91e8b8beaa7e41d1981bdfaf184625ba5eba1
TrashPanda <sean1teo1@gmail.com>
2015-09-16 22:56:36 -0400

    testing

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 //mongodb settings
 var dbSetting = {
   cookieSecret: 'a_blog_twitch',
-  url: "mongodb://testing:testing@ds041673.mongolab.com:41673/a_blog"
+  url: 'mongodb://testing:testing@ds041673.mongolab.com:41673/a_blog'
 };
 
 module.exports = dbSetting;
